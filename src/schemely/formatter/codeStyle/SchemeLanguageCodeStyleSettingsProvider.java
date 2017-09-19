2bbaf5296a8a661d858d3c4f9dcaff70174efdb3
Saigut <gosaigut@gmail.com>
2017-09-20 02:26:14 +0800

    fix Code style settings

--- src/schemely/formatter/codeStyle/SchemeLanguageCodeStyleSettingsProvider.java
+++ src/schemely/formatter/codeStyle/SchemeLanguageCodeStyleSettingsProvider.java
@@ -70,65 +70,6 @@ public class SchemeLanguageCodeStyleSettingsProvider extends LanguageCodeStyleSe
 
     @Override
     public IndentOptionsEditor getIndentOptionsEditor() {
-        return new SmartIndentOptionsEditor() {
-            private JTextField myLabelIndent;
-            private JLabel myLabelIndentLabel;
-
-            private JComboBox myLabelIndentStyle;
-            private JBLabel myStyleLabel;
-
-            @Override
-            public boolean isModified(final CodeStyleSettings settings, final CommonCodeStyleSettings.IndentOptions options) {
-                boolean isModified = super.isModified(settings, options);
-
-                isModified |= isFieldModified(myLabelIndent, options.LABEL_INDENT_SIZE);
-                isModified |= isLabelStyleModified(options.LABEL_INDENT_ABSOLUTE, settings.getCustomSettings(SchemeCodeStyleSettings.class).INDENT_LABEL_BLOCKS);
-                return isModified;
-            }
-
-            private boolean isLabelStyleModified(boolean absolute, boolean relative) {
-                if (absolute) {
-                    return !ABSOLUTE.equals(myLabelIndentStyle.getSelectedItem());
-                }
-                else if (relative) {
-                    return !RELATIVE.equals(myLabelIndentStyle.getSelectedItem());
-                }
-                else {
-                    return !RELATIVE_REVERSED.equals(myLabelIndentStyle.getSelectedItem());
-                }
-            }
-
-            @Override
-            public void apply(final CodeStyleSettings settings, final CommonCodeStyleSettings.IndentOptions options) {
-                super.apply(settings, options);
-                options.LABEL_INDENT_SIZE = getFieldValue(myLabelIndent, Integer.MIN_VALUE, options.LABEL_INDENT_SIZE);
-                options.LABEL_INDENT_ABSOLUTE = ABSOLUTE.equals(myLabelIndentStyle.getSelectedItem());
-                settings.getCustomSettings(SchemeCodeStyleSettings.class).INDENT_LABEL_BLOCKS = RELATIVE.equals(myLabelIndentStyle.getSelectedItem());
-            }
-
-            @Override
-            public void reset(@NotNull final CodeStyleSettings settings, @NotNull final CommonCodeStyleSettings.IndentOptions options) {
-                super.reset(settings, options);
-                myLabelIndent.setText(Integer.toString(options.LABEL_INDENT_SIZE));
-                if (options.LABEL_INDENT_ABSOLUTE) {
-                    myLabelIndentStyle.setSelectedItem(ABSOLUTE);
-                }
-                else if(settings.getCustomSettings(SchemeCodeStyleSettings.class).INDENT_LABEL_BLOCKS) {
-                    myLabelIndentStyle.setSelectedItem(RELATIVE);
-                }
-                else {
-                    myLabelIndentStyle.setSelectedItem(RELATIVE_REVERSED);
-                }
-            }
-
-            @Override
-            public void setEnabled(final boolean enabled) {
-                super.setEnabled(enabled);
-                myLabelIndent.setEnabled(enabled);
-                myLabelIndentLabel.setEnabled(enabled);
-                myStyleLabel.setEnabled(enabled);
-                myLabelIndentStyle.setEnabled(enabled);
-            }
-        };
+        return new SmartIndentOptionsEditor();
     }
 }
